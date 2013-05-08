995475653919076454353db2598b8904544e3bb8
unknown <rzurer@WT-WK-RZUR-02.CPEX.com>
2013-05-08 14:46:22 -0400

    refactored

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,112 +1,121 @@
 var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
-  monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  getDaysInMonth = function (month, year) {
-    if ((month === 1) && (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0))) {
-      return 29;
-    }
-    return daysInMonth[month];
-  },
-  test = function () {
-    var month, year, months, i, j, firstDayDate, firstDay;
-    month = 0;
-    year = 2013;
-    months = 12;
-    dayInYear = 1;
-    for (i = 0; i < months; i += 1) {
-      firstDayDate = new Date(year, month, 1);
-      firstDay = firstDayDate.getDay();
-      console.log(firstDay);
-      for (j = 0; j < 42; j += 1) {
-        if ((j < firstDay) || (j >= firstDay + getDaysInMonth(month, year))) {
-          continue;
-        }
-        console.log(monthNames[i] + ' ' + (j - firstDay + 1) + ' ' + dayInYear);
-        dayInYear += 1;
+    monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    getDaysInMonth = function (month, year) {
+      if ((month === 1) && (year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0))) {
+        return 29;
       }
-      month += 1;
-    }
-  };
-function renderCalendar(parent, year) {
-    var i, j, k, m, index, month, monthTable, monthTables, yearTable, row, cell, firstDayDate, firstDay, dayOfWeek, businessDaysInYear, dayInYear; 
-    month = 0;
-    businessDaysInYear = 0,
-    dayInYear = 1;
-    monthTables = [];
-    for (i = 0; i < 12; i += 1) {
-      monthTable = $('<table>');
-      monthTable.attr('id', 'calendarMonth'+ i);
+      return daysInMonth[month];
+    },
+    createMonthTableHeaderRow = function (monthName) {
+      var row, cell;
       row = $('<tr>');
       cell = $('<th>'); 
-      cell.text(monthNames[month]);
-      cell.css('font-size', '11px');
-      cell.css('color', 'black');
+      cell.addClass('monthTableHeader')
+      cell.text(monthName);
       cell.attr('colspan', 7);
       row.append(cell);
-      monthTable.append(row);
-      row = $('<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>');
-      row.css('color', 'blue')
-      row.css('vertical-align', 'top');
-      row.css('font-weight', 'normal')
-      row.css('line-height', '20px')
-      monthTable.append(row);
-      firstDayDate = new Date(year, month, 1);
-      firstDay = firstDayDate.getDay();
-      for (j = 0; j < 42; j += 1) {
-        if (j % 7 === 0){
-          row = $('<tr>'); 
-          monthTable.append(row);    
-        }
-        if ((j < firstDay) || (j >= firstDay + getDaysInMonth(month, year))) {
-          cell =$('<td>'); 
-          cell.text('');
-          row.append(cell);
-        } else {
-          cell =$('<td>'); 
-          cell.css('border', '1px solid gainsboro');
-          cell.css('vertical-align', 'top');
-          cell.css('width', '28px');
-          cell.css('height', '28px');
-          cell.text(j - firstDay + 1);
-          cell.attr('id', year + '|' + dayInYear);
-          dayInYear += 1;
-          dayOfWeek = new Date(year, month, j - firstDay).getDay();
-          if(dayOfWeek === 6 || dayOfWeek === 5){
-             cell.css('color', 'gainsboro');    
-          } else {
-            businessDaysInYear += 1;
-          }
-          row.append(cell);        
-        }
+      return row;
+    },
+    createDaysOfWeekHeaderRow = function () {
+       var row;
+       row = $('<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>');
+       row.addClass('daysOfWeekHeader');
+       return row;
+    },
+    createCalendarDayCell = function (id, text, isWeekendDay) {
+      var cell;
+      cell = $('<td>');
+      cell.attr('id', id);
+      cell.text(text);
+      cell.addClass("calendarCell");
+      if(isWeekendDay){
+        cell.addClass("weekendCell");
       }
-      month += 1;
-      monthTables.push(monthTable);
-   }
-    index = 0;
-    yearTable = $('<table>');
-    row = $('<tr>');
-    cell = $('<th>'); 
-    cell.text(year);
-    cell.css('font-size', '16px');
-    cell.attr('colspan', 3);
-    row.append(cell);
-    cell = $('<th>'); 
-    cell.text('Business Days: ' + businessDaysInYear);
-    cell.css('font-size', '12px');
-    cell.css('text-align', 'right');
-    row.append(cell);
-    yearTable.append(row);
-    for (k = 0; k < 3; k += 1) {
-      row = $('<tr>'); 
-      row.css('vertical-align', 'top');
-      for (m = 0; m < 4; m += 1) {
-         cell =$('<td>');
-         cell.css('border', '1px solid gainsboro');
-         cell.css('padding', '2px');
-         cell.html(monthTables[index]);
-         row.append(cell);
-         index += 1;
-      }
-      yearTable.append(row);
+      return cell;
+    },
+    createYearCell = function (year) {
+      var cell;
+      cell = $('<th>'); 
+      cell.text(year);
+      cell.addClass("yearCell");
+      cell.attr('colspan', 3);
+      return cell;
+    },
+    createYearInfoCell = function (businessDaysInYear) {
+      var cell;
+      cell = $('<th>'); 
+      cell.text('Business Days: ' + businessDaysInYear);
+      cell.addClass("yearInfoCell");
+      return cell;
+    },
+    createYearHeaderRow = function (year, businessDaysInYear) {
+      var row;
+      row = $('<tr>');
+      row.append(createYearCell(year));
+      row.append(createYearInfoCell(businessDaysInYear));
+      return row;
     }
-   parent.append(yearTable);
-}
+    createMonthContainerRow = function() {
+        var row;
+        row = $('<tr>'); 
+        row.addClass('monthContainerRow')
+        return row;
+    },
+    createMonthContainerCell = function (monthTable) {
+      var cell;
+      cell =$('<td>');
+      cell.addClass('monthContainerCell')
+      cell.html(monthTable);
+      return cell;
+    },
+    createCalendarFor = function (year) {
+      var monthTables, month, firstDay, monthTable, dayInYear, businessDaysInYear, dayOfWeek, isWeekendDay;
+      monthTables = [];
+      businessDaysInYear = 0,
+      month = 0;
+      for (i = 0; i < 12; i += 1) {
+        firstDay = new Date(year, month, 1).getDay();
+        monthTable = $('<table>');
+        monthTable.append(createMonthTableHeaderRow(monthNames[month]));
+        monthTable.append(createDaysOfWeekHeaderRow());
+        for (j = 0; j < 42; j += 1) {
+          if (j % 7 === 0){
+            row = $('<tr>'); 
+            monthTable.append(row);    
+          }
+          if ((j < firstDay) || (j >= firstDay + getDaysInMonth(month, year))) {
+            row.append($('<td>'));
+          } else {
+            dayInYear += 1;
+            dayOfWeek = new Date(year, month, j - firstDay).getDay();
+            isWeekendDay = dayOfWeek === 6 || dayOfWeek === 5;
+            if(!isWeekendDay){
+               businessDaysInYear += 1;
+            }
+            row.append(createCalendarDayCell(year + '|' + dayInYear, j - firstDay + 1, isWeekendDay));        
+          }
+        }
+        month += 1;
+        monthTables.push(monthTable);
+      }
+      return {businessDaysInYear : businessDaysInYear, monthTables: monthTables};
+    },
+    createYearTableFor = function(year){
+      var calendar, index, yearTable, row;
+      calendar = createCalendarFor(year)
+      index = 0;
+      yearTable = $('<table>');
+      yearTable.append(createYearHeaderRow(year, calendar.businessDaysInYear));
+      for (k = 0; k < 3; k += 1) {
+        row = createMonthContainerRow();
+        for (m = 0; m < 4; m += 1) {
+           row.append(createMonthContainerCell(calendar.monthTables[index]));
+           index += 1;
+        }
+        yearTable.append(row);
+      }
+      return yearTable;
+    },
+    renderCalendar = function(parent, year) {
+     parent.append(createYearTableFor(year));
+  };
